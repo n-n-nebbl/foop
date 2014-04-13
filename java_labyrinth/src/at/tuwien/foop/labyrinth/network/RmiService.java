88b7d0d13d9d4a46a2f8a9500a3ea80d1e9b2803
nebel <alex.nebel@gmail.com>
2014-04-13 15:26:50 +0200

    added class to start/stop/register for rmi

---
+++ java_labyrinth/src/at/tuwien/foop/labyrinth/network/RmiService.java
@@ -0,0 +1,16 @@
+package at.tuwien.foop.labyrinth.network;
+
+public class RmiService {
+
+	public boolean registerToServer() {
+		return false;
+	}
+	
+	public boolean startRegistry(LabyrinthServer server) {
+		return false;
+	}
+	
+	public void stopRegistry() {
+		
+	}
+}

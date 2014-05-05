98a2b48cd3f5b8d8873ab9b112d5667de7c15849
Cocomocodaloco <corinna.kirner@gmx.net>
2014-05-05 16:48:42 +0200

    refactor

--- java_labyrinth/src/main/java/at/tuwien/foop/labyrinth/gui/LabyrinthView.java
+++ java_labyrinth/src/main/java/at/tuwien/foop/labyrinth/gui/LabyrinthView.java
@@ -10,6 +10,7 @@ import javax.swing.Timer;
 import javax.swing.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import at.tuwien.foop.labyrinth.event.EventFactory;
 import at.tuwien.foop.labyrinth.event.EventHandler;
@@ -20,177 +21,108 @@ import at.tuwien.foop.labyrinth.model.Labyrinth;
 import at.tuwien.foop.labyrinth.model.Mouse;
 
 
-public class LabyrinthView extends JPanel implements ActionListener, ImageObserver{
+public class LabyrinthView extends Observable {
 
-	private Labyrinth f; 
-	private Mouse mouse_blue, mouse_green;
-	
-	@Autowired
-	private EventFactory eventFactory;
-	private MouseMoveEvent mouseEvent;
-	private EventHandler<MouseMoveEvent> eventHandler;
-	
+	private ClassPathXmlApplicationContext ctx;
+	private Labyrinth lab; 
+	private Mouse mouse_blue, mouse_green;	
 	private Timer timer;
+	private ImageIcon closedDoorImg, openedDoorImg;
+	private JButton doorButton;
+	private int clickedButtonID;
 	//private Timer timer2 = new Timer(500, new TimerListener());
 
+	@Resource(name="doorList")
+	private List<Door> doors;
+
+
 	public LabyrinthView() {
-		
-		f = new Labyrinth(31, "./maps/map.txt");
-		
-		mouse_blue = new Mouse(1, 1, "./images/player_blue.png");
-		mouse_green = new Mouse(29, 1, "./images/player_green.png");
-
-		eventFactory = new EventFactory();
-		mouseEvent = new MouseMoveEvent();
-		eventHandler = new MouseMoveEventHandler();
-		
-		addKeyListener(new Al());
-
-		setFocusable(true);
-		
-		//for the KeyListener
-		timer = new Timer(100, this);
-		timer.setRepeats(true);
-		timer.start();
-		
-		//timer2.start();
+		this.startLabyrinth();
 	}
 
-	/**
-	 * creates the graphical components for the map.
-	 */
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
+	public void startLabyrinth(){
+		JFrame f = new JFrame();
+		f.setTitle("Mouse Labyrinth");
+		f.add(new LabyrinthComponent());
+		f.setSize(620, 640);
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
-		for(int y=0; y<31; y++){
-			for(int x=0; x<31; x++){
-				if(f.getLabyrinth(x, y).equals("W")){
-					g.setColor(Color.black);
-					g.fillRect(x*20, y*20, 20, 20);
-				} 
-				else if(f.getLabyrinth(x, y).equals("P")){
-					g.setColor(Color.white);
-					g.fillRect(x*20, y*20, 20, 20);
-				}  
-				else if(f.getLabyrinth(x, y).equals("G")){
-					g.setColor(Color.yellow);
-					g.fillRect(x*20, y*20, 20, 20);
-				} 
-				else if(f.getLabyrinth(x, y).equals("D")){
-					final Icon closedDoorImg = new ImageIcon("./images/closed_door.png");  
-					final Icon openedDoorImg = new ImageIcon("./images/opened_door.png");
-				
-					final JButton doorButton = new JButton(closedDoorImg);
-					doorButton.setBounds(x*20, y*20, 20, 20); 
-					this.add(doorButton);
-					
-					doorButton.addActionListener(new ActionListener(){  
-						public void actionPerformed(ActionEvent e) {  
-							doorButton.setIcon(doorButton.getIcon() == closedDoorImg ? openedDoorImg : closedDoorImg);
-						}  
-					}); 
+	public class LabyrinthComponent extends JPanel implements ActionListener {
 
+		public LabyrinthComponent(){
+
+			ctx = new ClassPathXmlApplicationContext("application-context.xml");
+			doors = (List<Door>) ctx.getBean("doorList");
+
+			lab = new Labyrinth(31, "./maps/map.txt");
+			mouse_blue = new Mouse(1, 1, "./images/player_blue.png");
+			mouse_green = new Mouse(29, 1, "./images/player_green.png");
+			closedDoorImg = new ImageIcon("./images/closed_door.png");  
+			openedDoorImg = new ImageIcon("./images/opened_door.png");
+			doorButton = new JButton(closedDoorImg);
+			
+			
+//			timer = new Timer(100, this);
+//			timer.setRepeats(true);
+//			timer.start();
+
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			repaint();
+		}
+
+		/**
+		 * creates the graphical components for the map.
+		 */
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+
+			for(int y=0; y<31; y++){
+				for(int x=0; x<31; x++){
+					if(lab.getLabyrinth(x, y).equals("W")){
+						g.setColor(Color.black);
+						g.fillRect(x*20, y*20, 20, 20);
+					} 
+					else if(lab.getLabyrinth(x, y).equals("P")){
+						g.setColor(Color.white);
+						g.fillRect(x*20, y*20, 20, 20);
+					}  
+					else if(lab.getLabyrinth(x, y).equals("G")){
+						g.setColor(Color.yellow);
+						g.fillRect(x*20, y*20, 20, 20);
+					} 
+					else if(lab.getLabyrinth(x, y).equals("D")){
+						Door d = new Door();
+						doorButton = new JButton(closedDoorImg);
+						doors.add(d);
+						doorButton.setName(""+d.getId());
+						doorButton.setBounds(x*20, y*20, 20, 20);
+						doorButton.addMouseListener(new Ml());
+						this.add(doorButton);
+						
+					}
 				}
 			}
+			g.drawImage(mouse_blue.getImg(), mouse_blue.getX()*20, mouse_blue.getY()*20,20,20, null);
+			g.drawImage(mouse_green.getImg(), mouse_green.getX()*20, mouse_green.getY()*20,20,20, null);
+			
 		}
-		g.drawImage(mouse_blue.getImg(), mouse_blue.getX()*20, mouse_blue.getY()*20,20,20, null);
-		g.drawImage(mouse_green.getImg(), mouse_green.getX()*20, mouse_green.getY()*20,20,20, null);
-		repaint();
+		
 	}
 	
-	/**
-	 * TestCode for labyrinthsolve algorithm
-	 * TestKeyAdapter for checking if mouse moves like expected
-	 * TODO the mouse moves should work via the Events  
-	 * curaintly the view changes the model directly...
-	 */
-
-	public class Al extends KeyAdapter{
-
-		public void keyPressed(KeyEvent e){
-			int keycode = e.getKeyCode();
-
-			if(keycode == KeyEvent.VK_W){
-				if(f.getLabyrinth(mouse_blue.getX(), mouse_blue.getY()-1).equals("P"))
-					
-					mouse_blue.setX(mouse_blue.getX());
-				    mouse_blue.setY(mouse_blue.getY()-1);
-
-				//	eventFactory.createMouseMoveEvent(mouse_blue, 0, mouse_blue.getX(), mouse_blue.getY()-1);
-			}
-			if(keycode == KeyEvent.VK_A){
-				if(f.getLabyrinth(mouse_blue.getX()-1, mouse_blue.getY()).equals("P"))
-					
-					mouse_blue.setX(mouse_blue.getX()-1);
-				    mouse_blue.setY(mouse_blue.getY());
-
-				//	eventFactory.createMouseMoveEvent(mouse_blue, 3, mouse_blue.getX()-1, mouse_blue.getY());
-			}
-			if(keycode == KeyEvent.VK_S){
-				if(f.getLabyrinth(mouse_blue.getX(), mouse_blue.getY()+1).equals("P"))
-					
-					mouse_blue.setX(mouse_blue.getX());
-				    mouse_blue.setY(mouse_blue.getY()+1);
-
-				//	eventFactory.createMouseMoveEvent(mouse_blue, 2, mouse_blue.getX(), mouse_blue.getY()+1);
-			}
-			if(keycode == KeyEvent.VK_D){
-				if(f.getLabyrinth(mouse_blue.getX()+1, mouse_blue.getY()).equals("P"))
-					
-					mouse_blue.setX(mouse_blue.getX()+1);
-					mouse_blue.setY(mouse_blue.getY());
-					
-				//	eventFactory.createMouseMoveEvent(mouse_blue, 1, mouse_blue.getX()+1, mouse_blue.getY());
-					
-			}
-			System.out.println("Mouse Position: " + mouse_blue.getX() + " " + mouse_blue.getY());
-
-			if(mouse_blue.getX()==11 && mouse_blue.getY()==27)
-				System.out.println("FOUND CHEESEEEEEE!");
-
+	public class Ml extends MouseAdapter{
+		public void mouseClicked(MouseEvent e){
+			clickedButtonID = Integer.parseInt(e.getComponent().getName());
+			System.out.println("B " + clickedButtonID);
 		}
-
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		repaint();
-	}
-	
-	/**
-	 * TestCode for automated labyrinthsolve algorithm
-	 */
-
-	//	private class TimerListener implements ActionListener
-	//	{
-	//		public void actionPerformed(ActionEvent e)
-	//		{
-	//			
-	//			if(mouse.getTileX()!=11 && mouse.getTileY()!=27){
-	//				if(!m.getMap(mouse.getTileX()+1, mouse.getTileY()).equals("W") && !visitedPath.contains(new Path(mouse.getTileX()+1, mouse.getTileY()))){
-	//					mouse.moves(1, 0);
-	//					visitedPath.add(new Path(mouse.getTileX(), mouse.getTileY()));
-	//					System.out.println("MOUSE " + mouse.getTileX() + " " + mouse.getTileY());
-	//				}
-	//				else if(!m.getMap(mouse.getTileX(), mouse.getTileY()+1).equals("W") && !visitedPath.contains(new Path(mouse.getTileX(), mouse.getTileY()+1))){
-	//					mouse.moves(0, 1);
-	//					visitedPath.add(new Path(mouse.getTileX(), mouse.getTileY()));
-	//					System.out.println("MOUSE " + mouse.getTileX() + " " + mouse.getTileY());
-	//				}
-	//				else if(!m.getMap(mouse.getTileX()-1, mouse.getTileY()).equals("W") && !visitedPath.contains(new Path(mouse.getTileX()-1, mouse.getTileY()))){
-	//					mouse.moves(-1, 0);
-	//					visitedPath.add(new Path(mouse.getTileX(), mouse.getTileY()));
-	//					System.out.println("MOUSE " + mouse.getTileX() + " " + mouse.getTileY());
-	//				}
-	//			}
-	//			
-	//			System.out.println(visitedPath.size());
-	//		
-	//			repaint();
-	//
-	//
-	//		}
 }
+
 
 
 

2d4edb1eb9f34247de1fb88360f35320f8a5846a
Cocomocodaloco <corinna.kirner@gmx.net>
2014-05-05 16:48:03 +0200

    added mvc calls

--- java_labyrinth/src/main/java/at/tuwien/foop/labyrinth/StartLabyrinth.java
+++ java_labyrinth/src/main/java/at/tuwien/foop/labyrinth/StartLabyrinth.java
@@ -1,27 +1,42 @@
 package at.tuwien.foop.labyrinth;
 
+
 import java.rmi.RemoteException;
+
+import javax.swing.SwingUtilities;
 
 import org.springframework.beans.BeansException;
 
 import at.tuwien.foop.labyrinth.event.DoorClickedEvent;
 import at.tuwien.foop.labyrinth.event.EventBus;
 import at.tuwien.foop.labyrinth.event.MouseMoveEvent;
-import at.tuwien.foop.labyrinth.gui.GUI;
+import at.tuwien.foop.labyrinth.gui.LabyrinthController;
+import at.tuwien.foop.labyrinth.gui.LabyrinthModel;
+import at.tuwien.foop.labyrinth.gui.LabyrinthView;
 import at.tuwien.foop.labyrinth.model.Door;
 
 public class StartLabyrinth {
 
 	public static void main(String[] args) throws BeansException, RemoteException {
 		
-		new GUI();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {                                           
+                LabyrinthModel model = new LabyrinthModel();
+                LabyrinthView view = new LabyrinthView(); 
+                LabyrinthController controller = new LabyrinthController(model,view);
+                controller.control();
+            }
+        });  
 		
 		EventBus bus = ContextHolder.getContext().getBean(EventBus.class);
 
+		bus.fireEvent(new MouseMoveEvent());
 		bus.fireEvent(new MouseMoveEvent());
 		bus.fireEvent(new DoorClickedEvent(1, Door.DOOR_OPEN));
 		bus.fireEvent(new MouseMoveEvent());
 
 		ContextHolder.getContext().close();
+
 	}
 }

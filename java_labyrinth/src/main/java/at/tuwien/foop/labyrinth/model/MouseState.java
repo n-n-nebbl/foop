31b95bf6f49e6d73c19233de02831404fd1843a1
Angela <purker_angela@hotmail.com>
2014-06-17 21:49:35 +0200

    MouseState addes

---
+++ java_labyrinth/src/main/java/at/tuwien/foop/labyrinth/model/MouseState.java
@@ -0,0 +1,84 @@
+package at.tuwien.foop.labyrinth.model;
+
+import java.io.Serializable;
+
+import at.tuwien.foop.labyrinth.model.Mouse.MouseDirection;
+
+public class MouseState implements Serializable
+{
+	private static final long serialVersionUID = 8858297925796196801L;
+
+	private int x;
+	private int y;
+	private MouseDirection direction;
+
+	public MouseState(int x, int y, MouseDirection direction)
+	{
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+	}
+
+	public MouseState(Mouse mouse)
+	{
+		this.x = mouse.getState().getX();
+		this.y = mouse.getState().getY();
+		this.direction = mouse.getState().getDirection();
+	}
+
+	public void forward()
+	{
+		switch(direction)
+		{
+			case UP:
+				y--;
+				break;
+			case RIGHT:
+				x++;
+				break;
+			case LEFT:
+				x--;
+				break;
+			case DOWN:
+				y++;
+				break;
+		}
+	}
+
+	public MouseDirection getDirection()
+	{
+		return direction;
+	}
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public void setDirection(MouseDirection direction)
+	{
+		this.direction = direction;
+	}
+
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+
+	@Override
+	public String toString()
+	{
+		return x + " " + y + " " + direction;
+	}
+
+}

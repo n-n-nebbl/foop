ac4eb03396a50681082118f4c7f2c9953a61e0eb
simon <simon@Notebook>
2014-06-25 16:11:31 +0200

    Entry file added

---
+++ java_labyrinth/src/main/java/at/tuwien/foop/labyrinth/model/Entry.java
@@ -0,0 +1,12 @@
+package at.tuwien.foop.labyrinth.model;
+
+public class Entry extends Entity
+{
+
+	private static final long serialVersionUID = 2929861644939806887L;
+
+	public Entry()
+	{
+		super('E');
+	}
+}
